b46ce9a6bc277ae9d439585eecb9690f1ecd9239
Michela <micheladellagatta1@gmail.com>
2025-10-22 14:26:42 +0200

    Find student class by searching the name and log result in console

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,6 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const studentMarcoLanci = students.find(student => student.name === 'Marco Lanci');
+console.log(`La classe di Marco Lanci è la ${studentMarcoLanci.class}`);
